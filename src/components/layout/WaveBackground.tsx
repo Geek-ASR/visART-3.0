5dd4853a05ca4c7f245f900691f13766f1b9440c
Aditya Rekhe <adityarekhe1030@gmail.com>
2025-06-02 23:14:32 +0000

    the waves are too much at bottom, bring them slightly upwards

--- src/components/layout/WaveBackground.tsx
+++ src/components/layout/WaveBackground.tsx
@@ -24,7 +24,7 @@ const wavesConfig: WaveConfig[] = [
     pathD: "M0,192 C200,100 400,280 600,192 S1000,100 1200,192 S1400,280 1600,192 L1600,350 L0,350 Z",
     startColor: 'hsl(215 50% 92% / 0.6)', // Light secondary
     endColor: 'hsl(215 40% 96% / 0.3)',   // Very light secondary
-    initialTopVh: 55,
+    initialTopVh: 40, // Adjusted from 55
     heightVh: 50, 
     opacity: 0.8,
     parallaxFactor: 0.09,
@@ -36,7 +36,7 @@ const wavesConfig: WaveConfig[] = [
     pathD: "M0,224 C250,150 450,300 700,224 S950,150 1200,224 S1450,300 1600,224 L1600,350 L0,350 Z",
     startColor: 'hsl(260 60% 85% / 0.65)', // Light accent (purple)
     endColor: 'hsl(260 50% 92% / 0.35)',  // Very light accent
-    initialTopVh: 50,
+    initialTopVh: 35, // Adjusted from 50
     heightVh: 55,
     opacity: 0.9,
     parallaxFactor: 0.06,
@@ -48,7 +48,7 @@ const wavesConfig: WaveConfig[] = [
     pathD: "M0,256 C300,180 500,320 800,256 S1100,180 1400,256 S1700,320 1900,256 L1900,350 L0,350 Z",
     startColor: 'hsl(231 70% 80% / 0.7)', // Light primary (blue)
     endColor: 'hsl(231 60% 90% / 0.4)',   // Very light primary
-    initialTopVh: 45,
+    initialTopVh: 30, // Adjusted from 45
     heightVh: 60,
     opacity: 1,
     parallaxFactor: 0.03,
