ac5a203c4a2eaa4d05487fb2ccc0978d2b86e750
Aditya Rekhe <adityarekhe1030@gmail.com>
2025-06-02 19:49:55 +0000

    stillnot visible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type { Metadata } from 'next';
 import './globals.css';
 import { Navbar } from '@/components/layout/Navbar';
@@ -22,31 +23,33 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased" suppressHydrationWarning={true}>
+        {/* Global Wave Background */}
         <div aria-hidden="true" className="fixed inset-0 z-[-1] overflow-hidden">
           <div
             className="absolute rounded-full transform
                        top-[-80vh] left-[-50vw]
                        w-[200vw] h-[200vh]
-                       bg-secondary/30 
+                       bg-primary/20 
                        rotate-[-40deg]"
           />
           <div
             className="absolute rounded-full transform
                        bottom-[-70vh] right-[-60vw]
                        w-[220vw] h-[180vh]
-                       bg-primary/15 
+                       bg-accent/20 
                        rotate-[30deg]"
           />
           <div
             className="absolute rounded-full transform
                        top-[0vh] left-[-70vw]
                        w-[250vw] h-[150vh]
-                       bg-accent/15 
+                       bg-foreground/10
                        rotate-[-55deg]"
           />
         </div>
         
-        <div className="relative z-0 flex flex-col min-h-screen bg-background">
+        {/* Main Content Wrapper - Now transparent by default, waves will show through */}
+        <div className="relative z-0 flex flex-col min-h-screen">
           <Navbar />
           <main className="flex-grow container mx-auto px-4 py-8">
             {children}
