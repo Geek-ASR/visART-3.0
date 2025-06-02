150ddbf22cb42974f6af169a807bc2a1768beb75
Aditya Rekhe <adityarekhe1030@gmail.com>
2025-06-02 19:48:09 +0000

    where are the waves? i cant see them.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,35 +21,38 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased flex flex-col min-h-screen" suppressHydrationWarning={true}>
+      <body className="font-body antialiased" suppressHydrationWarning={true}>
         <div aria-hidden="true" className="fixed inset-0 z-[-1] overflow-hidden">
           <div
             className="absolute rounded-full transform
                        top-[-80vh] left-[-50vw]
                        w-[200vw] h-[200vh]
-                       bg-secondary/10
+                       bg-secondary/30 
                        rotate-[-40deg]"
           />
           <div
             className="absolute rounded-full transform
                        bottom-[-70vh] right-[-60vw]
                        w-[220vw] h-[180vh]
-                       bg-primary/05
+                       bg-primary/15 
                        rotate-[30deg]"
           />
           <div
             className="absolute rounded-full transform
                        top-[0vh] left-[-70vw]
                        w-[250vw] h-[150vh]
-                       bg-accent/05
+                       bg-accent/15 
                        rotate-[-55deg]"
           />
         </div>
-        <Navbar />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          {children}
-        </main>
-        <Footer />
+        
+        <div className="relative z-0 flex flex-col min-h-screen bg-background">
+          <Navbar />
+          <main className="flex-grow container mx-auto px-4 py-8">
+            {children}
+          </main>
+          <Footer />
+        </div>
         <Toaster />
       </body>
     </html>
